88fe8b104d0634629344c622df9d361c2d32e527
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-12 13:56:00 -0500

    added routes file

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// setting up routes
+app.use(require('./routes/apiRoutes.js'));
+app.use(require('./routes/htmlRoutes.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
