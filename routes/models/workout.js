f54f0d4c87708806df4e9649ad6687b5a1fb7a46
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-12 14:10:08 -0500

    workout.js model

--- routes/models/workout.js
+++ routes/models/workout.js
@@ -1,1 +1,45 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter exercise name"
+        },
+        duration: { 
+            type: Number,
+            default: 0,
+            required: "Enter exercise duration"
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number
+        }
+    }
+]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
