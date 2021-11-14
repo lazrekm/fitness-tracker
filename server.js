79d23eabda123ac6417541e8a9d18e1e5f4f08aa
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-14 17:06:43 -0500

    format

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require('morgan');
+const logger = require("morgan");
 
 // initialize server
 const app = express();
@@ -14,20 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-    { 
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // setting up routes
-app.use(require('./routes/apiRoutes.js'));
-app.use(require('./routes/htmlRoutes.js'));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
